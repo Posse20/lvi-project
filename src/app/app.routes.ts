eafc7b37768dde6d7c12a31cd2f3a37acffb113b
Mateo Parra <posse1512@gmail.com>
2025-05-14 22:45:51 -0500

    another change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { IlustrationsComponent } from './ilustrations/ilustrations.component';
+import { PicturesComponent } from './pictures/pictures.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'ilustrations', component: IlustrationsComponent}
+    {path: 'ilustrations', component: IlustrationsComponent},
+    {path: 'pictures', component: PicturesComponent}
 ];
