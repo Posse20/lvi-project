85ff8df66895a09f6f732fde6b64eccfec73b1fb
Posse20 <posse1512@gmail.com>
2025-07-07 17:53:07 -0500

    ilustrations tab readyyyyy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { IlustrationsComponent } from './ilustrations/ilustrations.component';
 import { PicturesComponent } from './pictures/pictures.component';
+import { ComicsComponent } from './comics/comics.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'ilustrations', component: IlustrationsComponent},
+    {path: 'comics', component: ComicsComponent},
     {path: 'pictures', component: PicturesComponent}
 ];
--- src/app/ilustrations/ilustrations.component.ts
+++ src/app/ilustrations/ilustrations.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'app-ilustrations',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ilustrations.component.html',
   styleUrl: './ilustrations.component.css'
 })
 export class IlustrationsComponent {
+  selectedImage: string | null = null;
 
+  openImage(imageUrl: string): void {
+    this.selectedImage = imageUrl;
+  }
+
+  closeImage(): void {
+    this.selectedImage = null;
+  }
 }
