3185463b95310d0fa9febbedf924761db3d6b5cd
Posse20 <posse1512@gmail.com>
2025-07-11 18:13:16 -0500

    about-me readyyyyyy

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './about-me.component.css'
 })
 export class AboutMeComponent {
+  carrouselImages = [
+    'assets/mamacita-1.jpg',
+    'assets/mamacita-2.jpg',
+    'assets/mamacita-3.jpg',
+    'assets/mamacita-4.jpg'
+  ]
+  currentImage = this.carrouselImages[0];
+  private index = 0;
 
+  constructor() {
+    setInterval(() => {
+      this.index = (this.index + 1) % this.carrouselImages.length;
+      this.currentImage = this.carrouselImages[this.index];
+    }, 3000); // cambia cada 3 segundos
+  }
 }
