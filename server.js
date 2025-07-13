f80610533f4956431e3904ebf4e36e3ee42598b2
Posse20 <posse1512@gmail.com>
2025-07-12 22:06:58 -0500

    change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 8080;
-const distPath = path.join(__dirname, 'dist/lvi-project');
+const distPath = path.join(__dirname, 'dist/lvi-project/browser');
 
 // Servir archivos estáticos
 app.use(express.static(distPath));
